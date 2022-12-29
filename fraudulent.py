5293662aedc06c88823ed95b77777e349c928f9d
VilakshanKh <31506996+Vilakshankh@users.noreply.github.com>
2022-12-29 17:44:22 -0330

    Encoding and Scaling changes, not working

--- fraudulent.py
+++ fraudulent.py
@@ -11,6 +11,8 @@ with open(sys.argv[1], 'r') as f:
 print(data)
 
 
+#Splitting
+
 #Split our dataset into its attributes and labels
 attributes = data.iloc[:, :-2].values #attributes
 labels = data.iloc[:, -2].values #labels
@@ -20,14 +22,55 @@ from sklearn.model_selection import train_test_split
 #Randomly select U=25 instances to be the instances in UnInstance
 attributes_train, attributes_test, labels_train, labels_test = train_test_split(attributes, labels, test_size = 0.25)
 
- 
-from sklearn.preprocessing import StandardScaler
-#scale the features so that all of them can be uniformly evaluated
-scaler = StandardScaler()
-scaler.fit(attributes_train)
 
-attributes_train = scaler.transform(attributes_train)
-attributes_test = scaler.transform(attributes_test)
+
+
+
+
+
+# Encoding
+
+from sklearn.preprocessing import LabelEncoder
+
+# Create a LabelEncoder object
+encoder = LabelEncoder()
+
+# Fit the encoder to the categorical data for the nameOrig column
+encoder.fit(data['nameOrig'])
+
+# Transform the nameOrig column in the training and test sets
+attributes_train[:, 3] = encoder.fit_transform(attributes_train[:, 3])
+attributes_test[:, 3] = encoder.fit_transform(attributes_test[:, 3])
+
+# Fit the encoder to the categorical data for the nameDest column
+encoder.fit(data['nameDest'])
+
+# Transform the nameDest column in the training and test sets
+attributes_train[:, 5] = encoder.fit_transform(attributes_train[:, 5])
+attributes_test[:, 5] = encoder.fit_transform(attributes_test[:, 5])
+
+
+
+
+# Scaling
+
+from sklearn.preprocessing import StandardScaler
+# Select the columns to be scaled (exclude the nameOrig and nameDest columns)
+attributes_train_scaled = attributes_train[:, [0, 1, 2, 4, 6, 7]]
+attributes_test_scaled = attributes_test[:, [0, 1, 2, 4, 6, 7]]
+
+# Scale the features
+scaler = StandardScaler()
+scaler.fit(attributes_train_scaled)
+
+attributes_train_scaled = scaler.transform(attributes_train_scaled)
+attributes_test_scaled = scaler.transform(attributes_test_scaled)
+
+
+
+
+
+#Training
 
 from sklearn.linear_model import LogisticRegression
 
@@ -42,6 +85,7 @@ labels_pred = model.predict(labels_test)
 
 
 
+#Precision
 
 from sklearn.metrics import precision_score
 
